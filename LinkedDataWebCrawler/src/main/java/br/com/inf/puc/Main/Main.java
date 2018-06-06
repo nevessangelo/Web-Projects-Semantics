9e0053439cc30f0a3dc18f35eaa01b54e58fb5d8
angelo <angelo@angelo-HP-Pavilion-Sleekbook-14-PC>
2018-06-05 23:15:51 -0300

    ok

--- LinkedDataWebCrawler/src/main/java/br/com/inf/puc/Main/Main.java
+++ LinkedDataWebCrawler/src/main/java/br/com/inf/puc/Main/Main.java
@@ -6,7 +6,6 @@
 package br.com.inf.puc.Main;
 
 import br.com.inf.puc.Search.VoIDSearch;
-import br.com.inf.puc.crawler.Triples;
 import java.util.ArrayList;
 
 /**
@@ -18,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting Crawler....");
         VoIDSearch voidsearch = new VoIDSearch();
-        voidsearch.Search("http://swlab.ic.uff.br/resource/id-6fbbb516-38a9-4a2b-bfbb-19d0ceffc1aa", 2);
+        voidsearch.Search("http://acm.rkbexplorer.com/id/1026296", 4);
         
         
     }
